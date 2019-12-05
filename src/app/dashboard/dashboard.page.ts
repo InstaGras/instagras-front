6e29524e6ad498925bdd1e7c632747325ed352f9
Théo Levalet <t.levalet@manitou.com>
2019-12-05 14:22:32 +0100

    feat: instagras feed header instagras text scroll to top

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import { KeycloakService } from '../auth/keycloak.service';
 import { UserdataService } from '../services/userdata.service';
+import { IonContent } from '@ionic/angular';
 
 
 
@@ -10,6 +11,7 @@ import { UserdataService } from '../services/userdata.service';
   styleUrls: ['dashboard.page.scss']
 })
 export class DashboardPage implements OnInit {
+  @ViewChild(IonContent, { static: false }) ionContent: IonContent;
 
   keycloakUserProfile: any;
   constructor(private keycloakService: KeycloakService, private UserdataService : UserdataService) {}
@@ -17,6 +19,10 @@ export class DashboardPage implements OnInit {
   ngOnInit(): void {
     this.keycloakUserProfile = this.keycloakService.getUserProfile();
     this.UserdataService.createUser(this.keycloakUserProfile.username);
+  }
+
+  scrollToTop() {
+    this.ionContent.scrollToTop(300);
   }
 
   logout(): void {
